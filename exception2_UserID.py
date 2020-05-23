6c37787c52b986e948df83c46e1397ecc2ea1c72
Parul Mathur <46438911+iamparul08@users.noreply.github.com>
2020-05-23 12:38:10 +0530

    Add files via upload

--- exception2_UserID.py
+++ exception2_UserID.py
@@ -1,11 +1,13 @@
-try:
-    num1_UserID = int(input("Enter 100"))
-    num2_UserID = int(input("Enter 0"))
 
+num1_UserID = int(input("Enter 100"))
+num2_UserID = int(input("Enter 0"))
+try:
+    print("\nResource open")
     div1_UserID = num1_UserID / num2_UserID
 
-except:
-    print("ZeroDivisionError is raised")
+except Exception as e:
+    print("Hey, you can not divide by zero", e)
+    print("\nResource closed.")
 
 finally:
     print("try catch ended.")
