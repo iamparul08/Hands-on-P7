6de7c0cd595ac31e154817403fe6a4d663bf5fe2
Parul Mathur <46438911+iamparul08@users.noreply.github.com>
2020-05-23 12:35:22 +0530

    file does not exist

--- exception1_UserID.py
+++ exception1_UserID.py
@@ -1,11 +1,13 @@
 try:
+    print("resource open")
     f = open('newfile1_UserID.txt', 'r')
     print(f.readlines())
+    f.close()
 
-except:
-    print("Exception occured i.e. FileNotFoundError")
+except Exception as e:
+    print("Hi,file is unavailable.", e)
+    print("resource closed")
 
-    
 finally:
     print("The 'try except' is finished.")
 
